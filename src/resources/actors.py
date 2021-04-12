009feb25baf0292e0e678e681fd2b972728bdff6
RGBSrs <grjanko.d.a@gmail.com>
2021-04-12 12:02:18 +0300

    route files refactoring

---
+++ src/resources/actors.py
@@ -0,0 +1,62 @@
+from flask.views import MethodView
+from flask import request, jsonify
+from marshmallow import ValidationError
+from src import db
+from src.models import Actor
+from src.schemas import ActorSchema
+
+
+class ActorListApi(MethodView):
+    actor_shema = ActorSchema()
+
+    def get(self, name = None):
+        if not name:
+            actors = db.session.query(Actor).all()
+            return jsonify(self.actor_shema.dump(actors, many = True)), 200
+        actor = db.session.query(Actor).filter_by(name = name).first()
+        if not actor:
+            return '', 404
+        return jsonify(self.actor_shema.dump(actor)), 200
+        
+    def post(self):
+        try:
+            actor = self.actor_shema.load(request.json, session = db.session)
+        except ValidationError as e:
+            return {'Message': f'error: {e}'}, 400
+        db.session.add(actor)
+        db.session.commit()
+        return jsonify(self.actor_shema.dump(actor)), 201
+
+    def put(self, name):
+        actor = db.session.query(Actor).filter_by(name = name).first()
+        if not actor:
+            return '', 404
+        try:
+            actor = self.actor_shema.load(request.json, instance = actor, session = db.session)
+        except ValidationError as e:
+            return {'Message': f'error: {e}'}, 400
+        db.session.add(actor)
+        db.session.commit()    
+        return jsonify(self.actor_shema.dump(actor)), 200
+
+    def patch(self, name):
+        actor = db.session.query(Actor).filter_by(name = name).first()
+        if not actor:
+            return '', 404
+        try:
+            actor = self.actor_shema.load(request.json, instance = actor, session = db.session,
+                partial = True)
+        except ValidationError as e:
+            return {'Message': f'error: {e}'}, 400
+        db.session.add(actor)
+        db.session.commit()    
+        return jsonify(self.actor_shema.dump(actor)), 200
+
+
+    def delete(self, name):
+        actor = db.session.query(Actor).filter(name == name).first()
+        if not actor:
+            return '', 404
+        db.session.delete(actor)
+        db.session.commit()
+        return '', 204
---
+++ src/resources/films.py
@@ -0,0 +1,63 @@
+from flask.views import MethodView
+from flask import request, jsonify
+from marshmallow import ValidationError
+from src import db
+from src.models import Film
+from src.schemas import FilmSchema
+
+
+class FilmListApi(MethodView):
+    film_shema = FilmSchema()
+
+    def get(self, uuid = None):
+        if not uuid:
+            films = db.session.query(Film).all()
+            return jsonify(self.film_shema.dump(films, many = True)), 200
+        film = db.session.query(Film).filter_by(uuid = uuid).first()
+        if not film:
+            return '', 404
+        return jsonify(self.film_shema.dump(film)), 200
+        
+    def post(self):
+        try:
+            film = self.film_shema.load(request.json, session = db.session)
+        except ValidationError as e:
+            return {'Message': f'error: {e}'}, 400
+        db.session.add(film)
+        db.session.commit()
+        return jsonify(self.film_shema.dump(film)), 201
+
+    def put(self, uuid):
+        film = db.session.query(Film).filter_by(uuid = uuid).first()
+        if not film:
+            return '', 404
+        try:
+            film = self.film_shema.load(request.json, instance = film, session = db.session)
+        except ValidationError as e:
+            return {'Message': f'error: {e}'}, 400
+        db.session.add(film)
+        db.session.commit()    
+        return jsonify(self.film_shema.dump(film)), 200
+
+    def patch(self, uuid):
+        film = db.session.query(Film).filter_by(uuid = uuid).first()
+        if not film:
+            return '', 404
+        try:
+            film = self.film_shema.load(request.json, instance = film, session = db.session,
+                partial = True )
+        except ValidationError as e:
+            return {'Message': f'error: {e}'}, 400
+        db.session.add(film)
+        db.session.commit()    
+        return jsonify(self.film_shema.dump(film)), 200
+
+
+    def delete(self, uuid):
+        film = db.session.query(Film).filter(uuid==uuid).first()
+        if not film:
+            return '', 404
+        db.session.delete(film)
+        db.session.commit()
+        return '', 204
+
