367f3f53f7f5e448b3dcd01fe57386c3ca0e019b
RGBSrs <grjanko.d.a@gmail.com>
2021-04-13 10:13:17 +0300

    auth token generation

--- src/resources/auth.py
+++ src/resources/auth.py
@@ -1,10 +1,14 @@
+import datetime
+import jwt
 from flask import request, jsonify
 from flask.views import MethodView
+from werkzeug.security import check_password_hash
 from marshmallow import ValidationError
 from sqlalchemy.exc import IntegrityError
 
+from src import db,app
 from src.schemas.users import UserSchema
-from src import db
+from src.database.models import User
 
 class AuthRegister(MethodView):
     user_schema = UserSchema()
@@ -20,3 +24,24 @@ class AuthRegister(MethodView):
         except IntegrityError:
             return {'Message' : 'Such user exists'}, 409
         return jsonify(self.user_schema.dump(user)), 201
+
+
+class AuthLogin(MethodView):
+    def get(self):
+        auth = request.authorization
+        if not auth:
+            return '', 401, {"WWW-Authenticate": "Basic realm='Authentication required'"}
+        user = db.session.query(User).filter_by(username = auth.get('username', ''))
+        if not user or not check_password_hash(user.password, auth.get('password','')):
+            return '', 401, {"WWW-Authenticate": "Basic realm='Authentication required'"}
+        token = jwt.encode(
+            {
+                'user_id' : user.uuid,
+                'exp' : datetime.datetime.now() + datetime.timedelta(hours=1)
+            }, app.config['SECRET_KEY']
+        )
+        return jsonify(
+            {
+                "token" : token.decode('utf-8')
+            }
+        )
