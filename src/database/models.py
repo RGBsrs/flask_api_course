5bc8793f1896f7eb521383715cf6cd1112d1db3a
RGBSrs <grjanko.d.a@gmail.com>
2021-04-13 09:26:16 +0300

    added user model

--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,5 @@
 import uuid
+from werkzeug.security import generate_password_hash
 from src import db
 
 
@@ -50,3 +51,23 @@ class Actor(db.Model):
 
     def __repr__(self):
         return f'Actor({self.name}, {self.birthday})' 
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(50), unique = True, nullable = False)
+    email = db.Column(db.String(50), unique = True, nullable = False)
+    password = db.Column(db.String(256), nullable = False)
+    is_admin = db.Column(db.Boolean, default = False)
+    uuid = db.Column(db.String(36), unique = True)
+
+    def __init__(self, username, email, password, is_admin = False):
+        self.username = username
+        self.email = email
+        self.password = generate_password_hash(password)
+        self.is_admin = is_admin
+        self.uuid = str(uuid.uuid4())
+
+    def __repr__(self):
+        return f'User: {self.username}, {self.email}, {self.is_admin}, {self.uuid}'
