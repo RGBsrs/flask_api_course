a00f3009b7e77ebbcce0bfa17796455ce196beb1
RGBSrs <grjanko.d.a@gmail.com>
2021-04-14 14:40:09 +0300

    requirements update

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import pathlib
 import os
 
 BASE_DIR = pathlib.Path(__file__).parent
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + f"{BASE_DIR}/data/db.sqlite"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1) or \
+        'sqlite:///' + f"{BASE_DIR}/data/db.sqlite"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'very secret key'
