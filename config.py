8740d070122687fe9ee1c7179b124f19d75a54dd
RGBSrs <grjanko.d.a@gmail.com>
2021-04-14 12:14:01 +0300

    add fixes to heroku release

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import pathlib
+import os
 
 BASE_DIR = pathlib.Path(__file__).parent
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + f"{BASE_DIR}/data/db.sqlite"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + f"{BASE_DIR}/data/db.sqlite"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'very secret key'
